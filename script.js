330f52a16e58ac763f243c3fa95dea1eacc7fe17
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-14 21:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function isSameType(value1, value2) {
-  //your js code here
+  if(type of value1===typeof value2){
+	  return true
+  }else{
+	  reture false
+  }
 }
 
 // do not change the code below.
